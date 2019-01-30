3b2b192ae71be2b6afd2f171d78ba7964a1ff015
Nicolas Ramoudt <nicolas.ramoudt@gmail.com>
2019-01-31 00:13:52 +0100

    Initial commit

---
+++ packages/smoetje/proggenerator/src/ProggeneratorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Smoetje\Proggenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProggeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
